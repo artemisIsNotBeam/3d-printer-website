722b608718e9a1d80cd20b8ed4f4e1927683a880
artemisbeams <66878297+artemisIsNotBeam@users.noreply.github.com>
2024-02-06 07:42:06 -0800

    footer now

--- script.js
+++ script.js
@@ -4,4 +4,10 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(data => {
             document.getElementById('header-placeholder').innerHTML = data;
         });
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data =>{
+            document.getElementById('footer-placeholder').innerHTML=data;
+        })
 });
+
